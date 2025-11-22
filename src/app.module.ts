0a80d3fdfe268b70a2b180d40e05a78863ef5d4a
minorscarf <104010115+minorscarf@users.noreply.github.com>
2025-11-22 23:49:32 +0900

    add: 必要なファイルを全作成
    nest g module <path>で作成できるので楽

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateModule } from './modules/create/create.module';
+import { ReadModule } from './modules/read/read.module';
+import { UpdateModule } from './modules/update/update.module';
+import { DeleteModule } from './modules/delete/delete.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [CreateModule, ReadModule, UpdateModule, DeleteModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/delete/delete.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { DeleteService } from './delete.service';
+import { DeleteController } from './delete.controller';
+
+@Module({
+  providers: [DeleteService],
+  controllers: [DeleteController]
+})
+export class DeleteModule {}
