511c0f264710e4688accf9086bbc0121103460e5
minorscarf <104010115+minorscarf@users.noreply.github.com>
2025-11-23 17:37:15 +0900

    update: Createの処理を実装
    create.controllerがリクエスト受け付けてcreate.serviceがDB操作してタスクを新規追加するという設計通りの実装
    ただし実際にDBに新規作成する処理の実装についてはエラーが出ていて未実装
    この辺について理解する必要がある
    prisma/service.ts
    repository/task.repository.ts
    この辺はエラーなので追加していない↑

--- src/modules/create/create.controller.ts
+++ src/modules/create/create.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateService } from './create.service';
+import { TaskDto } from 'src/dto/util.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
 
-@Controller('create')
-export class CreateController {}
+@Controller('tasks')
+export class CreateController {
+  constructor(private readonly createService: CreateService) {}
+
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto): Promise<TaskDto> {
+    return await this.createService.CreateTask(createTaskDto);
+  }
+}
--- src/modules/create/create.service.ts
+++ src/modules/create/create.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { TaskDto } from 'src/dto/util.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskRepository } from 'src/repository/task.repository';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class CreateService {}
+export class CreateService {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  async CreateTask(createTaskDto: CreateTaskDto): Promise<TaskDto> {
+    const taskId = randomUUID();
+
+    return await this.taskRepository.create({
+      id: taskId,
+      name: createTaskDto.name,
+      startDate: createTaskDto.startDate,
+      endDate: createTaskDto.endDate,
+      content: createTaskDto.content,
+    });
+  }
+}
