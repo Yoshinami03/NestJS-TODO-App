66f6f94a44dcb8705ebfa335b6b8adf4bdb37f99
minorscarf <104010115+minorscarf@users.noreply.github.com>
2025-11-23 20:01:42 +0900

    feat: DBからデータとってくる処理を実装した
    task.repository.tsに各処理を実装した
    
    readOne 指定したIDのタスクを取得する処理
    readAll タスクの一覧を取得する処理
    update  指定したIDのタスクを更新する処理
    delete  指定したIDのタスクを削除する処理

--- src/repository/task.repository.ts
+++ src/repository/task.repository.ts
@@ -17,12 +17,45 @@ export class TaskRepository {
       data,
     });
 
-    return {
-      id: task.id,
-      name: task.name,
-      startDate: task.startDate,
-      endDate: task.endDate,
-      content: task.content,
-    };
+    return task;
+  }
+
+  async readOne(id: string): Promise<TaskDto> {
+    const readTask = await this.prisma.task.findUnique({
+      where: { id },
+    });
+
+    if (!readTask) {
+      throw new Error(`Task with id ${id} not found`);
+    }
+
+    return readTask;
+  }
+
+  async readAll(): Promise<TaskDto[]> {
+    const taskList = await this.prisma.task.findMany();
+    return taskList;
+  }
+
+  async update(task: TaskDto): Promise<TaskDto> {
+    const updateTask = await this.prisma.task.update({
+      where: { id: task.id },
+      data: {
+        name: task.name,
+        startDate: task.startDate,
+        endDate: task.endDate,
+        content: task.content,
+      },
+    });
+
+    return updateTask;
+  }
+
+  async delete(id: string): Promise<TaskDto> {
+    const deleteTask = await this.prisma.task.delete({
+      where: { id },
+    });
+
+    return deleteTask;
   }
 }
