0cdcb10a5f6ec2ae9bc13a9b50947a7696a3a0ce
minorscarf <104010115+minorscarf@users.noreply.github.com>
2025-11-23 21:35:24 +0900

    feat: deleteを実装
    IDでタスクを指定して指定されたタスクを削除する処理

--- src/modules/delete/delete.controller.ts
+++ src/modules/delete/delete.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
+import { DeleteService } from './delete.service';
+import { TaskDto } from 'src/dto/util.dto';
 
-@Controller('delete')
-export class DeleteController {}
+@Controller('tasks')
+export class DeleteController {
+  constructor(private readonly deleteService: DeleteService) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<TaskDto> {
+    return await this.deleteService.DeleteTask(id);
+  }
+}
--- src/modules/delete/delete.service.ts
+++ src/modules/delete/delete.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { TaskDto } from 'src/dto/util.dto';
+import { TaskRepository } from 'src/repository/task.repository';
 
 @Injectable()
-export class DeleteService {}
+export class DeleteService {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  async DeleteTask(id: string): Promise<TaskDto> {
+    return await this.taskRepository.delete(id);
+  }
+}
